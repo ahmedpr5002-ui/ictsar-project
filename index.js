4ce86eca41d1799186033e029ee77cc79ac091a0
asdfg5587 <ahhhat44@gmail.com>
2026-05-31 22:43:03 +0300

    Update backend to serverless

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const serverless = require('serverless-http'); // إضافة هذه المكتبة
 const userRouter = require("./routes/user");
 const doctorRouter = require("./routes/doctor");
 const doctorAppointment = require("./routes/Appointment");
@@ -8,27 +9,18 @@ const app = express();
 var cors = require('cors');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
 connectDB();
 
-// إعدادات Middleware
 app.use('/uploadsUser', express.static(path.join(__dirname, 'uploadsUser')));
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors({ origin: '*' }));
 app.use('/uploads', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/user", userRouter);
 app.use("/Appointment", doctorAppointment);
 app.use("/doctor", doctorRouter);
 
-module.exports = app;
 
-if (require.main === module) {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+module.exports = app;
+module.exports.handler = serverless(app);
